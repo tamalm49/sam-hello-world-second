b30615444fe7858058173ba38ec50c7f433e1916
Tamal Mondal <tamalm49@gmail.com>
2026-08-28 18:03:52 +0530

    update response message in lambdaHandler to provide a clearer description

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -15,7 +15,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello world',
+                message: 'This is a sample Lambda function',
             }),
         };
     } catch (err) {
